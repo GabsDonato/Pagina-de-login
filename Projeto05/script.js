1998e37674fd91fe12a4a173b92f82a009482f30
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2023-10-24 16:02:48 -0300

    Initial commit

---
+++ Projeto05/script.js
@@ -0,0 +1,17 @@
+
+var usuario = "rntcursos@gmail.com";
+var password = "12345678";
+
+function check_login(){
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+
+    if(usuario == email && password == senha){
+        document.location.href = "login.html";
+    }else{
+        alert("Usuário ou senha errados")
+
+        document.getElementById("forget").style.display = "flex";
+    }
+}
